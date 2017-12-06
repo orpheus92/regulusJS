858ccd118392f635fdc552dedd34ebe9e48f70c4
orpheus92 <dwang0127@gmail.com>
2017-12-05 20:53:35 -0700

    Website working except for tooltip and div position

--- src/Crystal/Crystal.js
+++ src/Crystal/Crystal.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import * as d3Tip from 'd3-tip';
+import './style.css';
 
 
 export class Crystal {
--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3';
+import './style.css';
 
 
 export class Slider{
