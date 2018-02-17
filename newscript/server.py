f8462a91934c56099fb03d3948d8a9a8ff80c6b7
orpheus92 <dwang0127@gmail.com>
2018-02-16 17:13:59 -0700

    Initial Test with Websockets

---
+++ newscript/server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import asyncio
+import datetime
+import random
+import websockets
+
+def resimulate(info):
+    print("Resim") ##Using cyclus
+
+def dataextract():
+    print("Data")
+
+def calcMSC():
+    print("Calc MSC/PostProcesss")
+    return("Complete")
+async def updateDatabase(websocket, path):
+    print("Path = ")
+    #rint(path)
+    while True:
+        #now = datetime.datetime.utcnow().isoformat() + 'Z'
+        #await websocket.send(now)
+        #await websocket.recv()
+        #await asyncio.sleep(random.random() * 3)
+        info = await websocket.recv()
+        resimulate(info)
+        dataextract()
+        state = calcMSC()
+        await websocket.send(state)
+        #print("MSG: ")
+
+start_server = websockets.serve(updateDatabase, 'localhost', 8000)
+#start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- src/app/index.js
+++ src/app/index.js
@@ -64,6 +64,7 @@ function load(){
             d3.csv('../data/Final_Tree.csv', function (error, treedata){
                 d3.json('../data/Base_Partition.json', function (error, basedata) {
                     //console.log(rawdata);
+                    let ws = new WebSocket('ws://localhost:8000/');
                     let yattr = document.getElementById('y_attr').value;
                     let plottype = document.getElementById('plottype').value;
                     // Plot View Constructor
@@ -253,8 +254,8 @@ function load(){
                             [cur_selection,cur_node] = plots.highlight();
 
                             selectindex = new Set(cur_selection.map(obj=>obj.index));
-                            console.log(cur_selection)
-
+                            console.log(cur_selection);
+                            ws.send("Message");
 
                         });
 
