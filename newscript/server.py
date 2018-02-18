267c8b70cdcbb37574f062c2be42e744f2441255
Di Wang <orpheus92@dhcp233.sci.utah.edu>
2018-02-18 16:39:00 -0700

    Websocket working on Linux, will get back to it after related work part finished for paper

--- newscript/server.py
+++ newscript/server.py
@@ -15,19 +15,23 @@ def calcMSC():
     print("Calc MSC/PostProcesss")
     return("Complete")
 async def updateDatabase(websocket, path):
-    print("Path = ")
+    #print("Path = ")
     #rint(path)
     while True:
         #now = datetime.datetime.utcnow().isoformat() + 'Z'
         #await websocket.send(now)
         #await websocket.recv()
         #await asyncio.sleep(random.random() * 3)
-        await websocket.recv()
+        info = await websocket.recv()
+        print("Message Received!")
+        print(info)
+
+        await websocket.send("I got the message!")
         #resimulate(info)
         #dataextract()
         #state = calcMSC()
         #await websocket.send(state)
-        print("MSG: ")
+
 
 start_server = websockets.serve(updateDatabase, 'localhost', 1234)
 #start_server = websockets.serve(time, '127.0.0.1', 5678)
